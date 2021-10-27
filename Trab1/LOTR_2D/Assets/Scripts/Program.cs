e44971789f2acec3cbd8b077eee76489ee0dfffe
AlOzorio <39777406+AlOzorio@users.noreply.github.com>
2021-10-26 22:21:07 -0300

    Atualização no program.cs

---
+++ Trab1/LOTR_2D/Assets/Scripts/Program.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Program : MonoBehaviour
+{   
+    private Astar astar;
+    private GeneticSolverScript gss;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        astar = GetComponent<Astar>();
+        gss = GetComponent<GeneticSolverScript>();
+
+        StartCoroutine(astar.ThereAndBackAgain());
+    }
+
+    public void GetTime()
+    {
+        gss.Solve();
+        double bestTime = 1/gss.bestFitness;
+        double totalTime = bestTime + (double)astar.travelTime;
+        Debug.Log("Tempo total gasto na viagem: " + totalTime);
+    }
+}
